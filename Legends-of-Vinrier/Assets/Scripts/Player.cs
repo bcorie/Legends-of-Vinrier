ec9f2d612e6d82b0261bd72d04b60268ffa63ab6
Jack Herz (RIT Student) <jjh7240@rit.edu>
2023-10-05 13:44:39 -0400

    Update Player.cs

--- Legends-of-Vinrier/Assets/Scripts/Player.cs
+++ Legends-of-Vinrier/Assets/Scripts/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class Player : Unit
 {
     private Vector3 position;
-    private List<Item> inventory = new List<Item>();
+    private List<Item> inventory;
 
     public Player(string unitName, int unitLevel, int damage, int maxHP) : base(unitName, unitLevel, damage, maxHP)
     {
         this.position = Vector3.zero;
+        this.inventory = new List<Item>();
     }
 
     public Vector3 GetPosition()
@@ -16,8 +17,18 @@ public class Player : Unit
         return this.position;
     }
 
+    public List<Item> GetInventory()
+    {
+        return this.inventory;
+    }
+
     public void SetPosition(Vector3 position)
     {
         this.position = position;
+    }
+
+    public void InventoryAdd(Item item)
+    {
+        inventory.Add(item);
     }
 }

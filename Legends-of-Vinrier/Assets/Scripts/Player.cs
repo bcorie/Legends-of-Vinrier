4bc9cc620fe6579671b0ca6bc59fa71d30869200
Jack Herz (RIT Student) <jjh7240@rit.edu>
2023-10-05 13:36:12 -0400

    Added Inventory and Player Position

--- Legends-of-Vinrier/Assets/Scripts/Player.cs
+++ Legends-of-Vinrier/Assets/Scripts/Player.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Player : Unit
 {
+    private Vector3 position;
+
     public Player(string unitName, int unitLevel, int damage, int maxHP) : base(unitName, unitLevel, damage, maxHP)
     {
+        this.position = Vector3.zero;
+    }
 
+    public Vector3 GetPosition()
+    {
+        return this.position;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        this.position = position;
     }
 }
